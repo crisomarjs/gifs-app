52f225b4dbc309f0244f6b601e653cba11c9ed26
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-07-14 17:09:40 -0600

    Carga inicial de codigo y maquetacion

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { environment } from '@enviroments/environment'
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './gifs-side-menu-header.component.html'
+})
+export class GifsSideMenuHeaderComponent {
+
+  envs = environment;
+
+}
